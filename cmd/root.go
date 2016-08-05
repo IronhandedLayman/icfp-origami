0dc5cdd846fbf4b7388a39a3f70bcfb5e78045d0
Nathaniel Dean <nathaniel.dean@alumni.purdue.edu>
2016-08-04 21:35:31 -0400

    Added Cobra framework, redacted mention of apiKey in added docs

---
+++ cmd/root.go
@@ -0,0 +1,85 @@
+// Copyright © 2016 Nathaniel Dean
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "icfp-origami",
+	Short: "Sends commands and processes files for the ICFP 2016 competition",
+	Long:  `Sends commands and processes files for the ICFP 2016 competition`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.icfp-origami.yaml)")
+	RootCmd.PersistentFlags().StringVarP(&teamid, "teamid", "t", "", "id for team")
+	RootCmd.PersistentFlags().StringVarP(&apiKey, "apiKey", "k", "", "api key to access website")
+	RootCmd.PersistentFlags().StringVarP(&websiteLoc, "website", "h", "", "where to send API requests")
+
+	viper.BindPFlag("teamid", RootCmd.PersistentFlags().Lookup("teamid"))
+	viper.BindPFlag("apiKey", RootCmd.PersistentFlags().Lookup("apiKey"))
+	viper.BindPFlag("website", RootCmd.PersistentFlags().Lookup("website"))
+
+	viper.SetDefault("website", "2016sv.icfpcontest.org")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" { // enable ability to specify config file via flag
+		viper.SetConfigFile(cfgFile)
+	}
+
+	viper.SetConfigName(".icfp-origami") // name of config file (without extension)
+	viper.AddConfigPath("$HOME")         // adding home directory as first search path
+	viper.AutomaticEnv()                 // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
